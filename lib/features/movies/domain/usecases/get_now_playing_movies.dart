a38cc7c0495e077c36e63229f8a254f9a9ff255f
axel.legue <axel.legue31@gmail.com>
2020-07-12 21:27:45 +0200

    feat: add usecase get_now_playing_movies

---
+++ lib/features/movies/domain/usecases/get_now_playing_movies.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_star/core/error/failures.dart';
+import 'package:movie_star/core/usecases/use_case.dart';
+import 'package:movie_star/features/movies/domain/entities/movies.dart';
+import 'package:movie_star/features/movies/domain/repositories/movie_repository.dart';
+
+class GetNowPlayingMovies implements UseCase<Movies, NoParams> {
+  MovieRepository movieRepository;
+
+  GetNowPlayingMovies(this.movieRepository);
+
+  @override
+  Future<Either<Failure, Movies>> call(NoParams params) {
+    return movieRepository.getNowPlayingMovies();
+  }
+}
---
+++ test/features/movies/domain/usescases/get_now_playing_movies_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie_star/core/usecases/use_case.dart';
+import 'package:movie_star/features/movies/domain/entities/movies.dart';
+import 'package:movie_star/features/movies/domain/usecases/get_now_playing_movies.dart';
+
+import '../repository/mock_movie_repository.dart';
+
+void main() {
+  GetNowPlayingMovies getNowPlayingMovies;
+  MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    getNowPlayingMovies = GetNowPlayingMovies(mockMovieRepository);
+  });
+
+  final tMovies =
+      Movies(page: 1, totalPages: 100, totalResults: 1000, movies: []);
+
+  test(
+    'should get now playing movies from the repository',
+    () async {
+      // arrange
+      when(mockMovieRepository.getNowPlayingMovies()).thenAnswer(
+        (realInvocation) async => Right(tMovies),
+      );
+      // act
+      final result = await getNowPlayingMovies(NoParams());
+      // assert
+      expect(result, Right(tMovies));
+    },
+  );
+}
